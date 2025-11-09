c8463dfffeba79d852de770eacc23d9a4a139707
Jubaer Ahmed Faysal <157054403+JubaerFaysal@users.noreply.github.com>
2025-11-09 18:01:00 +0600

    rest api

--- MVC_Architecture/index.js
+++ MVC_Architecture/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 const routes = require("./routes/users_routes");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/sk")
+  .then(() => console.log("✅ DB connected"))
+  .catch((err) => console.log("❌", err));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
